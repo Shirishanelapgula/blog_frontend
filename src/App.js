245d7cb14a832da284703be37a2eea7890801133
shirishanelapogula <shirisha@godspeed.systems>
2024-02-04 22:46:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route , Routes} from "react-router-dom"
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import Header from "./Components/Header"
+import AddCategory from "./Pages/AddCategory";
+import AddBlog from "./Pages/AddBlog";
+import SingleBlog from "./Pages/SingleBlog";
+import PrivateRoute from "./Services/ProtectedRoutes"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+return (
+  <>
+  <Header/>
+  <Routes>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    
+    <Route path="/" element={<PrivateRoute/>}>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/add-blog" element={<AddBlog/>}/>
+    <Route path="/add-category" element={<AddCategory/>}/>
+    <Route path="/blog/:id" element={<SingleBlog/>}/>
+    </Route>
+  </Routes>
+  </>
+ )
 }
 
 export default App;
